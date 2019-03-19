51d742f937e94cb6ad8dba32629190666bb90354
wangfan <fan.wang@colorcun.com>
2019-03-19 18:57:28 +0800

    zh

--- app/Models/Elevator.php
+++ app/Models/Elevator.php
@@ -10,4 +10,21 @@ class Elevator extends Model
     use SoftDeletes;
 
     protected $table = 'elevator';
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'pid');
+    }
+    public function device()
+    {
+        return $this->hasOne(Device::class, 'did');
+    }
+    public function funcs()
+    {
+        return $this->hasMore(ElevatorFunc::class, 'did');
+    }
+    public function fitments()
+    {
+        return $this->hasMore(ElevatorFitment::class, 'did');
+    }
 }
