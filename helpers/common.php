856d9905997ba17310c74fd3c87647847e53d21a
wangfan <fan.wang@colorcun.com>
2019-05-23 15:57:40 +0800

    zh

--- helpers/common.php
+++ helpers/common.php
@@ -3,9 +3,33 @@
  *
  */
 function getBrand(){
-    echo "..........";
+    if(Admin::user()->isAdministrator()){
+        return '*';
+    }
+    if(Admin::user()->can('甲方集团') || Admin::user()->can('甲方地方')){
+        return '*';
+    }
+    $p=[];
+    foreach (Admin::user()->permissions as $per){
+        if(strpos($per->name,'电梯')>-1){
+            array_push($p,$per->slug);
+        }
+    }
+    return $p;
 }
 
 function getCity(){
-
+    if(Admin::user()->isAdministrator()){
+        return '*';
+    }
+    if(Admin::user()->can('甲方集团') || Admin::user()->can('乙方集团')){
+        return '*';
+    }
+    $p=[];
+    foreach (Admin::user()->permissions as $per){
+        if(strpos($per->name,'城市')>-1){
+            array_push($p,$per->slug);
+        }
+    }
+    return $p;
 }
